b496a800103ca5895f9c464b05667c41d929883a
samantas <samantas@nyu.edu>
2019-05-08 16:39:40 +0800

    random shapes triggered on scroll

--- js/script.js
+++ js/script.js
@@ -1,41 +1,15 @@
-// class Jitter {
-//     constructor(x, y, colorR, colorG, colorB) {
-//         this.x = x;
-//         this.y = y;
-//         this.colorR = colorR;
-//         this.colorG = colorG;
-//         this.colorB = colorB;
-//         this.diameter = random(30);
-//         this.speed = 2.8;
-//         this.dir = 1;
-//     }
-//     move() {
-//         this.x += random(-this.speed, this.speed);
-//         this.y += random(-this.speed, this.speed);
-//     }
-
-//     display() {
-//         noStroke();
-//         fill(this.colorR, this.colorG, this.colorB);
-//         ellipse(this.x, this.y, this.diameter, this.diameter);
-//     }
-// }
-
-
 class Shape {
-    constructor(x, y, height, width, className) {
+    constructor(x, y, height, width, shapeType) {
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
-        this.toggleClass = className;
-        this.borderRadius = Math.random();
+        this.shapeType = shapeType;
+        this.borderRadius = getRandomInt(0, 100);
     }
 
     display() {
-        // append a random shape
-
-        $('.container').append('<div class="' + className + '"></div>')
+        $('.container').append('<div class="shape ' + this.shapeType + '"></div>')
         $('.shape').css({
             "width": this.width,
             "height": this.height,
@@ -43,33 +17,40 @@ class Shape {
         });
     }
 
-    grow() {
-        $('.shape').css({
-            "width": "+= 100%",
-            "height": "+= 100%"
-        })
+    // grow() {
+    //     $('.shape').css({
+    //         "width": this.width + "+=100%",
+    //         "height": this.height + "+=100%"
+    //     })
+    // }
+
+    transform() {
+    	// rotate, translate, scale
     }
 }
 
+const shapeTypes = [
+    "circle",
+    "triangle",
+    "rectangle"
+];
+
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function init() {
     let shapes = [];
-    let classNames = ["circle", "rectangle", "triangle"];
-    let className = classNames[Math.floor(Math.random() * classNames.length)];
-
-    for (let i = 0; i < 5; i++) {
-        shapes[i] = new Shape(getRandomInt(0, 500), getRandomInt(0, 500), getRandomInt(0, 500), getRandomInt(0, 500), className);
-    }
 
     $(window).scroll(function() {
+        for (let i = 0; i < 10; i++) {
+            let shapeType = shapeTypes[Math.floor(Math.random() * shapeTypes.length)];
+            shapes[i] = new Shape(getRandomInt(0, 500), getRandomInt(0, 500), getRandomInt(0, 500), getRandomInt(0, 500), shapeType);
+        }
+
         for (let i = 0; i < shapes.length; i++) {
             shapes[i].display();
-            shapes[i].grow();
+            // shapes[i].grow();
         }
     });
 }
