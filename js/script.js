c6ad154d381575d18be79bc5f2186b3d121192db
samantas <samantas@nyu.edu>
2019-05-14 00:23:06 +0800

    small fix

--- js/script.js
+++ js/script.js
@@ -246,16 +246,20 @@ function fadeOutAtBottomOfPage() {
     wait = setTimeout(function() {
         window.onscroll = function(ev) {
             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                // you're at the bottom of the page
+                
                 $('.everything').animate({
                     opacity: 0
                 }, 1000);
+
                 toggleMusic();
                 song.muted = true;
 
                 $('.outro').animate({
                     opacity: 1
                 }, 1000);
+
+                $('.intro').css("display", "none");
+                $('.everything').css("display", "none");
             }
         };
     }, 1000);
